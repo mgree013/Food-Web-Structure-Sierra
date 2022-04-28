6a0d34b164f099ee4b00c533a5f615d07c93cf2b
mgree013 <mgree013@ucr.edu>
2022-04-28 10:03:15 -0600

    k

--- code/Mass.Abundance.R
+++ code/Mass.Abundance.R
@@ -27,7 +27,8 @@ species_all<-species%>%pivot_longer(-Site, names_to="Taxon", values_to="abundanc
 traits_mass<-traits%>%rename(Body_mass_mg=M)%>%dplyr::select(c(Taxon, Body_mass_mg))
 species_mass_data<-left_join(species_all,traits_mass, by="Taxon")
 
-species_mass_data_env<-left_join(species_mass_data,env, by="Site")
+species_mass_data_env<-left_join(species_mass_data,env, by="Site")%>%filter(O.NET != "YOUNG")
+########################################################################################################################
 #Create Mass abundance curves for each site, network, and regionally
 
 #1)Mass Abundance by Site
@@ -84,3 +85,52 @@ species_mass_data_env%>%
   geom_point()+
   geom_smooth(method = "lm")+
   facet_grid(~Order)
+
+
+
+########################################################################################################################
+#Part 2: CWM
+library(FD)
+row.traits<-traits%>%filter(Taxon !="Benthic.producers" & Taxon !="Pelagic.producers")
+traitsy<-traits%>%dplyr::rename(Body_mass_mg=M)%>%filter(Taxon !="Benthic.producers" & Taxon !="Pelagic.producers")%>%dplyr::select(c(Body_mass_mg))
+str(traitsy)
+rownames(traitsy)<-rownames(row.traits)
+
+tres_bm = dbFD(traitsy,species, corr = ("lingoes"),
+               stand.FRic = TRUE, calc.FDiv = TRUE)
+
+cwm=tres_bm$CWM
+FDis=as.data.frame(tres_bm$FDis)
+FEve=as.data.frame(tres_bm$FEve)
+FRic=as.data.frame(tres_bm$FRic)
+
+head(cwm)
+
+datas<-cbind(PC1,PC2,PC3,PC4,cwm,env,FRic,FDis,FEve)
+datas%>%
+  filter(River.dist.lake>0)%>%
+  gather(River.dist.lake,Head.river.dist,Chlorophyll.mean,Elevation,PC1,PC2,PC3,PC4,Discharge.Mean,Temp,DO,SHRUB_SCRUB,Local.Dist, key = "var", value = "value") %>% #PC2,PC3,PC4,River.dist.lake,
+  ggplot(aes(x = value, y = `tres_bm$FDis`))+ #, colour=as.factor(Fish))) + #remove , fill=Network and see what the grpah looks like, are there tredns that both entowrks share together
+  geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~ var, scales = "free") +
+  theme_bw()
+
+datas%>%
+  gather(Head.river.dist,River.dist.lake,key = "var", value = "value") %>% #PC2,PC3,PC4,River.dist.lake,
+  ggplot(aes(x = value, y = Body_mass_mg))+ #, colour=as.factor(Fish))) + #remove , fill=Network and see what the grpah looks like, are there tredns that both entowrks share together
+  geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~ var, scales = "free") +
+  theme_bw()
+
+
+dog<-lm(Body_mass_mg~Head.river.dist, datas)
+summary(dog)
+
+dog<-lm(Body_mass_mg~River.dist.lake, datas)
+summary(dog)
+
+
+#################################################
+
